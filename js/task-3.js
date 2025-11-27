4e2f8411578576fe78dc9eec873b088c439a8924
alinasavuyk <87060358+alinasavuyk@users.noreply.github.com>
2025-11-27 13:03:56 +0200

    js

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+const filterArray = function (numbers, value) {
+    let arrays = [];
+    for (let i = 0; i <= numbers.length; i++) {
+        const number = numbers[i];
+        if (number > value) {
+      arrays.push(number);
+        } 
+    }
+    return arrays;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
